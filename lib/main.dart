e25aa34a7e1b5480845e586a4256904a6436b5c8
TinasheG7 <tinashegatimu@gmail.com>
2025-05-04 13:46:00 +0300

    Created home-page branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:marikiti/core/constants/providers/splash_provider.dart';
-import 'package:marikiti/homepage.dart';
+//import 'package:marikiti/homepage.dart';            //--------------uncomment
 import 'package:marikiti/Widgets/splash_screen.dart';
 
 
-import 'package:marikiti/homepage.dart';
+//import 'package:marikiti/homepage.dart';        //--------------uncomment
 import 'package:marikiti/Widgets/pages/homepage_2.dart';
 // =======
 // import 'package:marikiti/core/constants/providers/product_provider.dart';
 // import 'package:marikiti/core/constants/providers/splash_provider.dart';
 // >>>>>>> main
-import 'package:marikiti/core/constants/View/auth/signup.dart';
+
+//import 'package:marikiti/core/constants/View/auth/signup.dart';  //--------------uncomment
 import 'package:marikiti/core/constants/providers/Checkoutprovider.dart';
 import 'package:marikiti/core/constants/providers/Subscriptionprovider.dart';
 import 'package:marikiti/core/constants/providers/theme_provider.dart';
@@ -73,11 +74,12 @@ class Marikiti extends StatelessWidget {
       initialRoute: '/splash_screen',
       // initialRoute: '/',
       routes: {
-        '/': (context) => SignUpScreen(),
-        '/splash_screen': (context) => SplashScreen(),
+        '/': (context) => HomePage2(),
+        //'/': (context) => SignUpScreen(),   //--------------uncomment
+        //'/splash_screen': (context) => SplashScreen(),   //--------------uncomment
 
-        '/home': (context) => HomePage(),
-        '/home2': (context) => HomePage2(),
+        //'/home': (context) => HomePage(),                //--------------uncomment
+        //'/home2': (context) => HomePage2(),
 // =======
 //         '/home': (context) => HomePage(),
 //         '/splash_screen': (context) => SplashScreen()
